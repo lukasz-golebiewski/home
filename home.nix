df8dbf2e18b6ec5fd2b894d888c8d53cdc64cc55
Łukasz Gołębiewski <lukasz.golebiewski@gmail.com>
2023-02-13 12:57:57 +0100

    Home manager state version = 22.11

--- home.nix
+++ home.nix
@@ -21,7 +21,7 @@ in {
   # You can update Home Manager without changing this value. See
   # the Home Manager release notes for a list of state version
   # changes in each release.
-  home.stateVersion = "22.05";
+  home.stateVersion = "22.11";
 
   home.packages = with pkgs; [
     dotfiles
