ad385f2cfefbf31a6d44823c915d77e018f62c86
Łukasz Gołębiewski <lukasz.golebiewski@gmail.com>
2023-03-14 23:31:05 +0100

    Bump emacs-overlay

--- common.nix
+++ common.nix
@@ -3,7 +3,7 @@
 let
   emacs-overlay = import (builtins.fetchTarball {
     url =
-      "https://github.com/nix-community/emacs-overlay/archive/c1355f8.tar.gz";
+      "https://github.com/nix-community/emacs-overlay/archive/5b3d956.tar.gz";
   });
 in {
   nixpkgs.overlays = [ emacs-overlay ];
