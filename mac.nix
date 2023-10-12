7f266a0091b812be04a6895d0f66a3e0c089d358
Łukasz Gołębiewski <lukasz.golebiewski@gmail.com>
2023-10-12 10:52:36 +0200

    Add mplayer to mac config

--- mac.nix
+++ mac.nix
@@ -1,10 +1,13 @@
 { config, pkgs, ... }:
-{
+let
+  mplayer = pkgs.mplayer.override {x264Support = true;};
+in {
   imports = [
     ./common.nix
   ];
   home.homeDirectory = "/Users/lukasz";
   home.packages = with pkgs; [
+     mplayer
      ntfs3g
    ];
 }
