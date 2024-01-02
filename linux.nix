b408965df4de06abfe1238206fc3e4a2095e96e6
Łukasz Gołębiewski <lukasz.golebiewski@gmail.com>
2024-01-02 15:21:34 +0100

    do not use outdated keybase

--- linux.nix
+++ linux.nix
@@ -5,13 +5,13 @@ in {
   imports = [
     ./common.nix
   ];
-  services.keybase.enable = true;
+  # services.keybase.enable = true;
 
   home.homeDirectory = "/home/lukasz";
 
   home.packages = with pkgs; [
      # kazam
-     keybase-gui
+     # keybase-gui
      mplayer
      peek
    ];
