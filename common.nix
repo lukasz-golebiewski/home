e863e0a9e0ef50e280470b5ec5893768c2de3a40
Łukasz Gołębiewski <lukasz.golebiewski@gmail.com>
2024-12-06 09:07:07 +0100

    Adds nix-community cachix

--- common.nix
+++ common.nix
@@ -54,7 +54,16 @@ in {
 
   nix = {
     package = pkgs.nix;
-    settings.experimental-features = [ "nix-command" "flakes" ];
+    settings = {
+      substituters = [
+        "https://cache.nixos.org/"
+        "https://nix-community.cachix.org"
+      ];
+      trusted-public-keys = [
+        "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+      ];
+      experimental-features = [ "nix-command" "flakes" ];
+    };
   };
 
   programs.vscode = {
