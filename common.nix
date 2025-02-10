099a70f4eaba1b040c5c5bc466940e4c5615e03d
Łukasz Gołębiewski <lukasz.golebiewski@gmail.com>
2025-02-10 20:10:29 +0100

    add cache.nixos.org trusted public key

--- common.nix
+++ common.nix
@@ -21,9 +21,10 @@ in {
   # You can update Home Manager without changing this value. See
   # the Home Manager release notes for a list of state version
   # changes in each release.
-  home.stateVersion = "24.05";
+  home.stateVersion = "24.11";
   home.packages = with pkgs; [
     awscli2
+    #buf
     cachix
     direnv
     dotfiles
@@ -60,6 +61,7 @@ in {
         "https://nix-community.cachix.org"
       ];
       trusted-public-keys = [
+        "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
         "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
       ];
       experimental-features = [ "nix-command" "flakes" ];
