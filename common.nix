336ac2bd96797797c4d19c650644107758676825
Łukasz Gołębiewski <lukasz.golebiewski@gmail.com>
2025-09-01 08:33:17 +0200

    bunch of vscode keybindings

--- common.nix
+++ common.nix
@@ -70,7 +70,6 @@ in {
       experimental-features = [ "nix-command" "flakes" ];
     };
   };
-
   programs.vscode = {
     enable = true;
     profiles = {
@@ -88,6 +87,13 @@ in {
           ms-azuretools.vscode-docker
           ms-kubernetes-tools.vscode-kubernetes-tools
         ];
+        keybindings = [
+        {
+          key = "k";
+          command = "cursorUp";
+          when = "resourceScheme == magit && vim.active && vim.mode == 'Normal'";
+        }
+        ];
         userSettings = {
           "vim.leader" = "<space>";
           "vim.normalModeKeyBindingsNonRecursive" = [
@@ -95,7 +101,30 @@ in {
               before = ["<leader>" "l" "f" "d"];
               commands = ["editor.action.revealDefinition"];
             }
-          # Add more multi-key leader sequences here if needed
+            {
+              before = ["<leader>" "x" "c"];
+              commands = ["workbench.action.showCommands"];
+            }
+            {
+              before = ["<leader>" "x" "o"];
+              commands = ["workbench.action.nextEditor"];
+            }
+            {
+              before = ["<leader>" "x" "p"];
+              commands = ["workbench.action.previousEditor"];
+            }
+            {
+              before = ["<leader>" "x" "q"];
+              commands = ["workbench.action.quit"];
+            }
+            {
+              before = ["<leader>" "s"];
+              commands = ["workbench.action.files.save"];
+            }
+            {
+              before = ["<leader>" "g" "s"];
+              commands = ["magit.status"];
+            }
           ];
           "files.watcherExclude" = {
             "**/.bloop" = true;
