30c2b94a312f13f17d8adbbcacbbc213ed9b0729
Łukasz Gołębiewski <lukasz.golebiewski@gmail.com>
2026-09-19 10:45:06 +0200

    cachix

--- common.nix
+++ common.nix
@@ -76,12 +76,14 @@
       substituters = [
         "https://cache.nixos.org/"
         "https://nix-community.cachix.org"
+        "https://lukasz-golebiewski-home.cachix.org"
         "https://cache.iog.io"
         "https://devenv.cachix.org"
       ];
       trusted-public-keys = [
         "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
         "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+        "lukasz-golebiewski-home.cachix.org-1:oroQLUpUfWrZjuodfgUchHYHITAZoZ15hbioIEyF6VY="
         "hydra.iohk.io:f/Ea+s+dFdN+3Y/G+FDgSq+a5NEWhJGzdjvKNGv0/EQ="
         "devenv.cachix.org-1:w1cLUi8dv3hnoSPGAuibQv+f9TZLr6cv/Hm9XgU50cw="
       ];
